c0e3e7a7a6a72633e4af5c991dc9e1c94cbd93d7
Leonardo Bento <sismico@gmail.com>
2021-07-27 21:58:51 -0300

    Versao com Join de Streams

--- src/main/kotlin/br/com/sismico/kafkastreamsexample/stream/StreamExample.kt
+++ src/main/kotlin/br/com/sismico/kafkastreamsexample/stream/StreamExample.kt
@@ -1,17 +1,15 @@
 package br.com.sismico.kafkastreamsexample.stream
 
-import br.com.sismico.kafkastreamsexample.producer.ClientProducer
 import br.com.sismico.kafkastreamsexample.producer.ProducerTopics
 import org.apache.kafka.streams.StreamsBuilder
 import org.apache.kafka.streams.Topology
+import org.apache.kafka.streams.kstream.JoinWindows
 import org.apache.kafka.streams.kstream.KStream
-import org.apache.kafka.streams.kstream.KTable
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.stereotype.Component
-import java.util.*
+import java.time.Duration
 
 @Configuration
 class StreamExample {
@@ -20,10 +18,25 @@ class StreamExample {
     @Bean
     fun build(): Topology {
         val builder = StreamsBuilder()
+
         val clients: KStream<String, String> = builder.stream(ProducerTopics.CLIENT.topic)
-        val counter: KTable<String, Long> = clients.groupBy { _, value -> value }
-            .count()
-        counter.toStream().foreach { key, value -> log.info("User: $key - Count: $value") }
+        val address: KStream<String, String> = builder.stream(ProducerTopics.ADDRESS.topic)
+
+//        val counter: KTable<String, Long> = clients.groupBy { _, value -> value }
+//            .count()
+//        counter.toStream().foreach { key, value -> log.info("User: $key - Count: $value") }
+
+        val kaddress = address
+            .selectKey { _, value -> value.split(":")[0] }
+            .mapValues { map -> map.split(":")[1] }
+        val kclient = clients.selectKey { _, value -> value }
+
+        kclient.join(
+            kaddress,
+            { left: String, right: String -> "$left/$right" },
+            JoinWindows.of(Duration.ofSeconds(10L))
+        ).to("sismico.combine")
+
         return builder.build()
     }
 
