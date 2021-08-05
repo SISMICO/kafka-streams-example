8c8cd7d3d5f0528580053b8232b1eaac86cf7175
Leonardo Bento <sismico@gmail.com>
2021-08-04 22:17:19 -0300

    Enable Kafka

--- kafka-flink-spring/src/main/java/br/com/sismico/kafkaflink/KafkaFlinkApplication.java
+++ kafka-flink-spring/src/main/java/br/com/sismico/kafkaflink/KafkaFlinkApplication.java
@@ -6,6 +6,7 @@ import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.co.ProcessJoinFunction;
+import org.apache.flink.streaming.api.functions.sink.SinkFunction;
 import org.apache.flink.streaming.api.windowing.assigners.EventTimeSessionWindows;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
@@ -20,9 +21,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KafkaFlinkApplication implements CommandLineRunner {
 
     private static final Logger log = LoggerFactory.getLogger(KafkaFlinkApplication.class);
-    private final NetCatConfig consumerConfig;
+    //private final NetCatConfig consumerConfig;
+    private final ConsumerConfig consumerConfig;
 
-    public KafkaFlinkApplication(NetCatConfig consumer) {
+    //public KafkaFlinkApplication(NetCatConfig consumer) {
+    public KafkaFlinkApplication(ConsumerConfig consumer) {
         this.consumerConfig = consumer;
     }
 
@@ -35,27 +38,29 @@ public class KafkaFlinkApplication implements CommandLineRunner {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         DataStream<Client> clientStream = consumerConfig.getClient(env)
+                .rebalance()
                 .map(value -> new Client(value));
         //.keyBy(value -> value.getClientId());
         DataStream<Address> addressStream = consumerConfig.getAddress(env)
+                .rebalance()
                 .map(value -> {
                     String[] valueList = value.split(":");
                     return new Address(valueList[0], valueList[1]);
                 });
         //.keyBy(value -> value.getClientId());
 
-//        clientStream.addSink(new SinkFunction<Client>() {
-//            @Override
-//            public void invoke(Client value, Context context) throws Exception {
-//                log.info(value.getClientId().toString());
-//            }
-//        });
-//        addressStream.addSink(new SinkFunction<Address>() {
-//            @Override
-//            public void invoke(Address value, Context context) throws Exception {
-//                log.info(value.getAddress());
-//            }
-//        });
+        clientStream.addSink(new SinkFunction<Client>() {
+            @Override
+            public void invoke(Client value, Context context) throws Exception {
+                log.info(value.getClientId().toString());
+            }
+        });
+        addressStream.addSink(new SinkFunction<Address>() {
+            @Override
+            public void invoke(Address value, Context context) throws Exception {
+                log.info(value.getAddress());
+            }
+        });
 
 //        DataStream<String> result = clientStream.join(addressStream)
 //                .where(value -> value.getClientId())
@@ -64,9 +69,10 @@ public class KafkaFlinkApplication implements CommandLineRunner {
 //                .apply((client, address) -> client.getClientId() + " - " + address.getAddress());
 
         DataStream<String> result = clientStream
+                .rebalance()
                 .keyBy(value -> value.getClientId())
                 .intervalJoin(addressStream.keyBy(value -> value.getClientId()))
-                .between(Time.seconds(-10), Time.seconds(10))
+                .between(Time.seconds(-10), Time.seconds(60))
                 .process(new ProcessJoinFunction<Client, Address, String>() {
                     @Override
                     public void processElement(Client client, Address address, ProcessJoinFunction<Client, Address, String>.Context ctx, Collector<String> out) throws Exception {
