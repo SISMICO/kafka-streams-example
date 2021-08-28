aae9096326f3e51ad62430c58b584df32ea1ff11
Leonardo Bento <sismico@gmail.com>
2021-08-28 00:13:29 -0300

    Add named stream and set one partition per topic

--- kafka-streams/src/main/kotlin/br/com/sismico/kafkastreamsexample/stream/KTableExample.kt
+++ kafka-streams/src/main/kotlin/br/com/sismico/kafkastreamsexample/stream/KTableExample.kt
@@ -2,16 +2,18 @@ package br.com.sismico.kafkastreamsexample.stream
 
 import org.apache.kafka.streams.StreamsBuilder
 import org.apache.kafka.streams.Topology
-import org.apache.kafka.streams.kstream.*
+import org.apache.kafka.streams.kstream.JoinWindows
+import org.apache.kafka.streams.kstream.KStream
+import org.apache.kafka.streams.kstream.KTable
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.time.Duration
 
-@Configuration
-class StreamExample {
-    val log: Logger = LoggerFactory.getLogger(StreamExample::class.java)
+//@Configuration
+class KTableExample {
+    val log: Logger = LoggerFactory.getLogger(KTableExample::class.java)
 
     @Bean
     fun build(): Topology {
@@ -24,17 +26,21 @@ class StreamExample {
 //            .count()
 //        counter.toStream().foreach { key, value -> log.info("User: $key - Count: $value") }
 
-        val kaddress = address
+        val kaddress: KTable<String, String> = address
             .selectKey { _, value -> value.split(":")[0] }
-            .mapValues( { v -> v.split(":")[1] }, Named.`as`("address_named"))
-        val kclient = clients.selectKey({ _, value -> value }, Named.`as`("client_named"))
+            .toTable()
+            .mapValues { map -> map.split(":")[1] }
+        val kclient = clients.selectKey { _, value -> value }.toTable()
 
-        kclient.join(
-            kaddress,
-            { left: String, right: String -> "$left/$right" },
-            JoinWindows.of(Duration.ofMinutes(10L)),
-            StreamJoined.`as`("combine_named")
-        ).to("sismico.combine")
+//        kclient.join(
+//            kaddress,
+//            { left: String, right: String -> "$left/$right" },
+//            JoinWindows.of(Duration.ofMinutes(10L))
+//        ).to("sismico.combine")
+
+        kclient
+            .join(kaddress, { left: String, right: String -> "$left/$right" })
+            .to("sismico.combine")
 
         return builder.build()
     }
