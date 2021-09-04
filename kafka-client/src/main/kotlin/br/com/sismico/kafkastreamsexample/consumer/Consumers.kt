558ddf119d40217fadf07d63d41676c1d99e87e4
Leonardo Bento <sismico@gmail.com>
2021-09-04 15:20:34 -0300

    Fix Stream to subscribe new topics

--- kafka-client/src/main/kotlin/br/com/sismico/kafkastreamsexample/consumer/Consumers.kt
+++ kafka-client/src/main/kotlin/br/com/sismico/kafkastreamsexample/consumer/Consumers.kt
@@ -1,8 +1,10 @@
 package br.com.sismico.kafkastreamsexample.consumer
 
 import br.com.sismico.kafkastreamsexample.config.CUSTOMER_CREATED
+import br.com.sismico.kafkastreamsexample.config.CUSTOMER_SEGMENTED
 import br.com.sismico.kafkastreamsexample.config.SCORE_CALCULATED
 import com.sismico.kafkastreamsexample.CustomerCreated
+import com.sismico.kafkastreamsexample.CustomerSegmented
 import com.sismico.kafkastreamsexample.ScoreCalculated
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -14,14 +16,19 @@ import org.springframework.stereotype.Service
 class Consumers {
     val log: Logger = LoggerFactory.getLogger(Consumers::class.java)
 
-    @KafkaListener(topics = [CUSTOMER_CREATED])
-    fun customer(customer: CustomerCreated) {
-        log.info("[RECEIVED] Event Received: Customer Created: ${customer.getUsername()} / ${customer.getDocument()}")
-    }
+//    @KafkaListener(topics = [CUSTOMER_CREATED])
+//    fun customerCreated(customer: CustomerCreated) {
+//        log.info("[RECEIVED] Event Received: Customer Created: ${customer.getUsername()} / ${customer.getDocument()}")
+//    }
 
     @KafkaListener(topics = [SCORE_CALCULATED])
-    fun customer(score: ScoreCalculated) {
+    fun scoreCalculated(score: ScoreCalculated) {
         log.info("[RECEIVED] Event Received: Score Calculated: ${score.getDocument()}: ${score.getScore()}")
+    }
+
+    @KafkaListener(topics = [CUSTOMER_SEGMENTED])
+    fun customerSegmented(segmentation: CustomerSegmented) {
+        log.info("[RECEIVED] Event Received: Customer Segmented: ${segmentation.getUsername()}: ${segmentation.getSegment()}")
     }
 
 }
--- kafka-streams/src/main/kotlin/br/com/sismico/kafkastreamsexample/stream/StreamExample.kt
+++ kafka-streams/src/main/kotlin/br/com/sismico/kafkastreamsexample/stream/StreamExample.kt
@@ -1,8 +1,13 @@
 package br.com.sismico.kafkastreamsexample.stream
 
-import br.com.sismico.kafkastreamsexample.entity.Address
-import br.com.sismico.kafkastreamsexample.entity.Client
+import br.com.sismico.kafkastreamsexample.config.CUSTOMER_CREATED
+import br.com.sismico.kafkastreamsexample.config.CUSTOMER_SEGMENTED
+import br.com.sismico.kafkastreamsexample.config.SCORE_CALCULATED
+import br.com.sismico.kafkastreamsexample.segment.SegmentService
 import com.sismico.kafka.Combined
+import com.sismico.kafkastreamsexample.CustomerCreated
+import com.sismico.kafkastreamsexample.CustomerWithScore
+import com.sismico.kafkastreamsexample.ScoreCalculated
 import org.apache.kafka.streams.StreamsBuilder
 import org.apache.kafka.streams.Topology
 import org.apache.kafka.streams.kstream.*
@@ -13,31 +18,36 @@ import org.springframework.context.annotation.Configuration
 import java.time.Duration
 
 @Configuration
-class StreamExample {
+class StreamExample(
+    val segmentService: SegmentService
+) {
     val log: Logger = LoggerFactory.getLogger(StreamExample::class.java)
 
     @Bean
     fun build(): Topology {
         val builder = StreamsBuilder()
 
-        val customer: KStream<String, com.sismico.kafka.Client> = builder.stream(ProducerTopics.CLIENT.topic)
-        val address: KStream<String, com.sismico.kafka.Address> = builder.stream(ProducerTopics.ADDRESS.topic)
+        // Subscribe to primary topics
+        val customer: KStream<String, CustomerCreated> = builder.stream(CUSTOMER_CREATED)
+        val score: KStream<String, ScoreCalculated> = builder.stream(SCORE_CALCULATED)
 
-//        val counter: KTable<String, Long> = clients.groupBy { _, value -> value }
-//            .count()
-//        counter.toStream().foreach { key, value -> log.info("User: $key - Count: $value") }
+        // Map a key to join the streams
+        val streamCustomer = customer
+            .selectKey({ _, value -> value.getDocument() }, Named.`as`("customer_document_key"))
+        val streamScore = score
+            .selectKey({ _, value -> value.getDocument() }, Named.`as`("score_document_key"))
 
-        val kaddress = address
-            .selectKey({ _, value -> value.getUsername() }, Named.`as`("address_named"))
-        val kclient = clients.selectKey({ _, value -> value.getUsername() }, Named.`as`("client_named"))
-
-        kclient.join(
-            kaddress,
-            { left: com.sismico.kafka.Client, right: com.sismico.kafka.Address -> Combined(left.getUsername(), right.getAddress(), "${left.getUsername()}/${right.getAddress()}") },
+        // Join Customer with Score to Segment the Customer
+        streamCustomer.join(
+            streamScore,
+            { left: CustomerCreated, right: ScoreCalculated ->
+                segmentService.segment(left, right)
+            },
             JoinWindows.of(Duration.ofMinutes(10L)),
-            StreamJoined.`as`("combine_named")
-        ).to("sismico.combine")
+            StreamJoined.`as`("customer_with_score")
+        ).to(CUSTOMER_SEGMENTED)
 
+        // Build and run ...
         return builder.build()
     }
 
